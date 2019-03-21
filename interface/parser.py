4a4454c0520e2b16630c4b53a9067d518a85b917
Tom Renner <tommrenner@gmail.com>
2019-03-21 13:46:04 +0000

    Add tests and beginning of implementation for user input parser.

--- interface/parser.py
+++ interface/parser.py
@@ -2,15 +2,38 @@
 class Parser():
 
     # TODO: castling, enpassant, etc. where the movestr is different
-    def get_move(self, string):
-        s = string.split('')
+    def get_move(self, string, board):
+        s = string.split(' ')
         sx = string.split('x')
+        possible_pieces = []
         if(len(s) == 2):
-            return _get_move_default(s)
+            possible_pieces = self._possible_pieces_on_board(s[0], board)
         elif(len(sx) == 2):
-            return _get_move_default(s, True)
+            possible_pieces =  self._possible_pieces_on_board(sx[0], board)
         else:
-            # pawn move - parse a little differently
-            return
+            possible_pieces = self._possible_pieces_on_board('P', board)
+        return self._find_user_move(string, possible_pieces, board)
+
+    def _possible_pieces_on_board(self, user_string_parts, board):
+        # Maybe....
+        # ... find pieces of right kind from board (new argument to init?) and check if any of their valid_moves is the same as that constructed here by user input?
+            # 1. for all pieces, p.str() == user_string_parts[0]
+            # 2. for all pices of the right kind:
+            # 3. do any of that piece's valid moves have the same move
+            #       string as entered by the user
+        possible_pieces = []
+        for p in board.pieces:
+            if(p.is_white == board.to_play):
+                if(p.str().upper() == user_string_parts[0]):
+                    possible_pieces.append(p)
+        return possible_pieces
+
+
+    def _find_user_move(self, user_move_string, possible_pieces, board):
+        for p in possible_pieces:
+            for m in p.get_valid_moves(board):
+                if(m.str() == user_move_string):
+                    return m
+
 
 
--- interface/test/test_parser.py
+++ interface/test/test_parser.py
@@ -1,9 +1,30 @@
 import unittest
 import interface.parser as parser
+import engine.board as board
 
 class ParserTest(unittest.TestCase):
 
-    def test_exists(self):
-        p = parser.Parser()
+    def setUp(self):
+        self.parser = parser.Parser()
+        self.board = board.Board()
 
+    def test_piece_move(self):
+        user_move = 'N c3'
+        move = self.parser.get_move(user_move, self.board)
+        self.assertEqual(move.str(), user_move)
+
+    def test_invalid_move_destination(self):
+        user_move = 'N c4'
+        move = self.parser.get_move(user_move, self.board)
+        self.assertEqual(move, None)
+
+    def test_wrong_colour_moved(self):
+        user_move = 'N c6'
+        move = self.parser.get_move(user_move, self.board)
+        self.assertEqual(move, None)
+
+    def test_pawn_move(self):
+        user_move = 'd4'
+        move = self.parser.get_move(user_move, self.board)
+        self.assertEqual(user_move, move.str())
 
