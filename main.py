62928442601d98b13e1df5319a6e5fe871be03a3
Tom Renner <tommrenner@gmail.com>
2019-12-19 20:41:06 +0000

    User as white can now play against naive decision making computer!!

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import readline
 import engine.board as Board
+import engine.tree as Tree
 import interface.parser as Parser
-
+import decision.naive as Naive
 
 board = Board.Board()
+tree = Tree.Tree()
+tree.set_depth(2)
 parser = Parser.Parser()
+naive = Naive.Naive()
 while(True):
     print(board.render())
     
+    # User move (plays white)
     user_input = str(input("Play a move: "))
     move = parser.get_move(user_input, board)
     while(move is None):
         user_input = str(input("Sorry, that's not a valid move - try again: "))
         move = parser.get_move(user_input, board)
     move.apply(board)
+    print(board.render())
 
+    # Engine move (plays black)
+    computer_move = naive.get_best_move(board)
+    computer_move.apply(board)
